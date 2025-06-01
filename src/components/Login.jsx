585d3b26ebf1f3d866e3f08b7bdb426198ad176d
piyushmraut <pmraut1515@gmail.com>
2025-06-01 11:47:13 +0530

    Started working on netflixgpt

---
+++ src/components/Login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+
+function Login() {
+  return (
+    <div>
+      <Header />
+      <img
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/af2fac72-d956-4952-8686-4d45d359d78c/web/IN-en-20250526-TRIFECTA-perspective_5db3e163-56f7-47c7-9a65-b79b9d76bf24_small.jpg"
+        alt="background image"
+      />
+    </div>
+  );
+}
+
+export default Login;
